a60b84b4e57201bf96f46b10c9bbb1b468879bdd
BT_2N2_13 <sample@bths.edu>
2023-09-21 13:22:45 -0400

    added a working while loop to calculate the total

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
+            double cost = 0;
 
             System.out.println("Welcome to the tip calculator!");
             System.out.println("How many people are in your group?");
@@ -10,5 +11,17 @@ public class Main {
             System.out.println("What's your tip percentage?");
             int percentage = scan.nextInt();
             System.out.println("Enter the cost in dollars and cents. (-1 to end)");
+            cost = scan.nextDouble();
+            double total = cost;
+            if(cost!= -1) {
+                while (cost != -1) {
+                    System.out.println("Enter the cost in dollars and cents. (-1 to end)");
+                    cost = scan.nextDouble();
+                    total = total + cost;
+                }
+            }
+            total = total + 1;
+            System.out.println("------------------");
+            System.out.println(total);
         }
     }
