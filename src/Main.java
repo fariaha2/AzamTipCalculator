c0b62c3c8d3675d5a2b2acd3abaffae55af0e07a
Fariah Azam <fariaha124@gmail.com>
2023-09-27 18:23:15 -0400

    added feature that calculates the total number of items ordered

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
             double cost = 0;
+            int numOfItem = 0;
+            int totalNumOfItem = 0;
 
             System.out.println("Welcome to the tip calculator!");
             System.out.println("How many people are in your group?");
@@ -13,11 +15,19 @@ public class Main {
             System.out.println("Enter the cost in dollars and cents. (-1 to end)");
             cost = scan.nextDouble();
             double total = cost;
+            System.out.println("How many of this item did you order?");
+            numOfItem = scan.nextInt();
+            totalNumOfItem = numOfItem;
             if(cost!= -1) {
                 while (cost != -1) {
                     System.out.println("Enter the cost in dollars and cents. (-1 to end)");
                     cost = scan.nextDouble();
-                    total = total + cost;
+                    if(cost!= -1) {
+                        System.out.println("How many of this item did you order?");
+                        numOfItem = scan.nextInt();
+                        totalNumOfItem = totalNumOfItem+numOfItem;
+                        total = total + cost*numOfItem;
+                    }
                 }
             }
             total = total + 1;
@@ -25,7 +35,9 @@ public class Main {
             double billWithTip = ((double) ((int) ((totalTip + total)*100))) / 100;
             double beforeTip = ((double) ((int) ((total/numOfPeople)*100))) / 100;
             double tipPerPerson = ((double) ((int) ((totalTip/numOfPeople)*100))) / 100;
-            double costPerPerson = tipPerPerson+beforeTip;
+            double costPerPerson = ((double) ((int) ((beforeTip+tipPerPerson)*100))) / 100;
+            System.out.println("-----------------");
+            System.out.println("Total number of items ordered: " + totalNumOfItem);
             System.out.println("-----------------");
             System.out.println("Total bill before tip: " + total);
             System.out.println("Total percentage: " + percentage);
